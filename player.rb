47ec6854f9859f4614679bc84a932cd0ae32ab3f
Luke Arndt <luke.j.arndt@gmail.com>
2013-08-07 14:19:59 +1000

    Level 7 solution

--- player.rb
+++ player.rb
@@ -3,7 +3,7 @@ class Player
   def play_turn(warrior)
     @warrior = warrior
     observe_before_turn
-    #ponder_loudly
+    ponder_loudly
     take_action!
     observe_after_turn
   end
@@ -11,14 +11,12 @@ class Player
   def initialize
     @last_health ||= 20
     @cleared_behind = false
-    @facing = :backward
   end
 
   def observe_before_turn
     @health = @warrior.health
-    @last_health ||= @health
-    @facing = :forward if @warrior.feel(:backward).wall?
-    @ahead = @warrior.feel(@facing)
+    @ahead = @warrior.feel
+    @behind = @warrior.feel(:backward)
   end
 
   def observe_after_turn
@@ -26,31 +24,37 @@ class Player
   end
 
   def take_action!
-    if @ahead.empty?
+    if @ahead.wall?
+      turn_around!
+    elsif @ahead.empty?
       walk_carefully!
     else
-      @warrior.attack!(@facing) if @ahead.enemy?
-      @warrior.rescue!(@facing) if @ahead.captive?
+      @warrior.attack! if @ahead.enemy?
+      @warrior.rescue! if @ahead.captive?
     end
   end
 
+  def turn_around!
+    @warrior.pivot!
+  end
+
   def walk_carefully!
-      if taking_damage?
+    if taking_damage?
         if healthy_enough?
-          @warrior.walk!(@facing)
+          @warrior.walk!
         else
-          @warrior.walk!(behind_direction)
+          @warrior.walk!(:backward)
         end
       elsif full_health?
-        @warrior.walk!(@facing)
+        @warrior.walk!
       else
         @warrior.rest!
       end
   end
 
   def ponder_loudly
-    puts "Forward: #{@forward}"
-    puts "Backward: #{@backward}"
+    puts "Ahead: #{@ahead}"
+    puts "Behind: #{@warrior.feel(:backward)}"
     puts "Taking damage? #{taking_damage?}"
   end
 
@@ -62,12 +66,9 @@ class Player
     @warrior.health == 20
   end
 
-  def behind_direction
-    @facing == :backward ? :forward : :backward
-  end
-
   def healthy_enough?
     @last_health == 20
   end
+
 
 end
